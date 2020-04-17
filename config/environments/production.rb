eb6c40fadd7884a4be81f0ac3bb3a4ff38847afb
catmando <mitch@catprint.com>
2020-04-17 01:11:28 -0400

    setup production action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://ceaselessprayer.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://ceaselessprayer.herokuapp.com', 'http://ceaselessprayer.herokuapp.com']
+
 end
